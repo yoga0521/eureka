3b97114fad206dc8b2ebb2c834224fd99b10250f
yoga0521 <627469339@qq.com>
2018-05-06 13:18:51 +0800

    添加EurekaTransportConfig默认值注释

--- eureka-client/src/main/java/com/netflix/discovery/shared/transport/EurekaTransportConfig.java
+++ eureka-client/src/main/java/com/netflix/discovery/shared/transport/EurekaTransportConfig.java
@@ -8,21 +8,21 @@ package com.netflix.discovery.shared.transport;
 public interface EurekaTransportConfig {
 
     /**
-     * Eureka-Client 会话重新连接的时间间隔，单位为秒
+     * Eureka-Client 会话重新连接的时间间隔，单位为秒，默认为20*60s
      *
      * @return the reconnect inverval to use for sessioned clients
      */
     int getSessionedClientReconnectIntervalSeconds();
 
     /**
-     * 请求失败的Eureka-Client隔离集合占Eureka-Client总数的占比，超过该比例会进行清空
+     * 请求失败的Eureka-Client隔离集合占Eureka-Client总数的占比，超过该比例会进行清空，默认为0.66
      *
      * @return the percentage of the full endpoints set above which the quarantine set is cleared in the range [0, 1.0]
      */
     double getRetryableClientQuarantineRefreshPercentage();
 
     /**
-     * 获取应用解析器数据过时时间间隔
+     * 获取应用解析器数据过时时间间隔，默认为5*60s
      *
      * @return the max staleness threshold tolerated by the applications resolver
      */
@@ -32,28 +32,28 @@ public interface EurekaTransportConfig {
      * By default, the applications resolver extracts the public hostname from internal InstanceInfos for resolutions.
      * Set this to true to change this behaviour to use ip addresses instead (private ip if ip type can be determined).
      *
-     * 应用解析器是否改用ip，默认情况下是从instance获取公共主机名
+     * 应用解析器是否改用ip，默认情况下是从instance获取公共主机名，默认为false
      *
      * @return false by default
      */
     boolean applicationsResolverUseIp();
 
     /**
-     * 异步解析 EndPoint 集群频率，单位为毫秒。
+     * 异步解析 EndPoint 集群频率，单位为毫秒。默认为5*60*1000ms
      *
      * @return the interval to poll for the async resolver.
      */
     int getAsyncResolverRefreshIntervalMs();
 
     /**
-     * 异步解析器预热解析 EndPoint 集群超时时间，单位为毫秒。
+     * 异步解析器预热解析 EndPoint 集群超时时间，单位为毫秒。默认为5000s
      *
      * @return the async refresh timeout threshold in ms.
      */
     int getAsyncResolverWarmUpTimeoutMs();
 
     /**
-     * 异步线程池大小
+     * 异步线程池大小，默认为5
      *
      * @return the max threadpool size for the async resolver's executor
      */
@@ -62,7 +62,7 @@ public interface EurekaTransportConfig {
     /**
      * The remote vipAddress of the primary eureka cluster to register with.
      *
-     * 获取要写入到eureka集群的远程vip
+     * 获取要写入到eureka集群的远程vip，默认为null
      *
      * @return the vipAddress for the write cluster to register with
      */
@@ -72,7 +72,7 @@ public interface EurekaTransportConfig {
      * The remote vipAddress of the eureka cluster (either the primaries or a readonly replica) to fetch registry
      * data from.
      *
-     * 获取eureka集群的远程vip地址
+     * 获取eureka集群的远程vip地址，默认为null
      *
      * @return the vipAddress for the readonly cluster to redirect to, if applicable (can be the same as the bootstrap)
      */
@@ -85,7 +85,7 @@ public interface EurekaTransportConfig {
      *    and warm (see {@link #getApplicationsResolverDataStalenessThresholdSeconds()}, otherwise
      *    fall back to a backing default
      *
-     * 获取bootstrap解析器的策略
+     * 获取bootstrap解析器的策略，默认为null
      *
      * @return null for the default strategy, by default
      */
@@ -98,7 +98,7 @@ public interface EurekaTransportConfig {
      * via {@link #getReadClusterVip()}. This indirect resolver may or may not return the same
      * targets as the bootstrap servers depending on how servers are setup.
      *
-     * 使用bootstrap解析器进行查询
+     * 使用bootstrap解析器进行查询，默认为true
      *
      * @return true by default.
      */
